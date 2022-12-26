14a2720e8ebe67a7b0fa5c93d57dcda1417541cf
Ele <ele.starshade@gmail.com>
2022-12-26 04:44:31 +0000

    chore: fix response

--- functions/starling-get-transactions.js
+++ functions/starling-get-transactions.js
@@ -27,7 +27,7 @@ exports.handler = async function (event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: data.feedItems })
+      body: JSON.stringify(data.feedItems)
     }
   } catch (error) {
     return {
