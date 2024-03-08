2c6cd93960a7002e4221f20add810be90beec46b
Ele <61023752+ele-starshade@users.noreply.github.com>
2024-03-08 19:07:43 +0000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div>Welcome to ele.codes... this is a work in progress for random learning that I'm doing :)</div>
+  <div>Welcome to fabo.live - This is a surprise, don't tell him.</div>
 </template>
 
 <style>
